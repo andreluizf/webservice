67d75e4fcdf26af6e123933ad73461938d72bf59
andreluizf <ferreira.andreluiz@gmail.com>
2014-01-24 08:58:41 -0200

    inicio

---
+++ src/com/nutricao/models/Banda.java
@@ -0,0 +1,29 @@
+package com.nutricao.models;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Banda {
+	private String nome;
+	private int anoDeFormacao;
+	private int id;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getAnoDeFormacao() {
+		return anoDeFormacao;
+	}
+	public void setAnoDeFormacao(int anoDeFormacao) {
+		this.anoDeFormacao = anoDeFormacao;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
---
+++ src/com/nutricao/ws/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.nutricao.ws;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/helloworld")
+public class HelloWorld {
+	@GET
+	@Produces("text/plain")
+	public String showHelloWorld() {
+		return "Olá mundo!";
+	}
+}
